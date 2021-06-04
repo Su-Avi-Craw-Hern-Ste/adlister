b3193f5a34eb0cafb49a3d39fdfb0cd3b609358b
Brett Crawford <brett.crawford57@gmail.com>
2021-06-04 16:42:07 -0500

    feat: seeder

---
+++ seeder.sql
@@ -0,0 +1,37 @@
+
+
+INSERT INTO users (username, email, password, money, class, phone_number, image)
+VALUES 	('brett', 'brettcrawford123@gmail.com', 'encrypted_password', 50000, 'Hunter', 2109876543, url:'localblahblah'),
+          ('shanshan' 'shanshan123@gmail.com', 'encrypted_password', 50000, 'Healer', 2108765432, url:'localblahblah2'),
+          ('alex', 'alex123@gmail.com', 'encrypted_password', 50000, 'Mage', 2107654321, url:'localblahblah3'),
+          ('manni', 'manni123@gmail.com', 'encrypted_password', 50000, 'Assassin', 2106543210, url:'localblahblah4');
+
+INSERT INTO categories (category)
+VALUES	('items'),
+          ('materials'),
+          ('animals'),
+          ('weapons');
+
+INSERT INTO images (image)
+VALUES	('dummy_url'),
+          ('dummy_url2'),
+          ('dummy_url3'),
+          ('dummy_url4');
+
+INSERT INTO ads (user_id, title, price, rarity, description)
+VALUES	(1, 'Legendary Cup for Sale', 3500, 'Legendary', 'This legendary cup is legendary. It can hold anything'),
+          (2, 'Selling Common Sword', 500, 'Common', 'Found this sword on the side of the road. Just looking for some money'),
+          (3, 'Somebody come get this dragon!', 23500, 'Unique', 'Adolescent blue dragon. Rebellious. Experienced dragon owners only'),
+          (4, 'White Carbon Soot', 1500, 'Uncommon', 'Smithing material. Excellent for crafting armor pieces.');
+
+INSERT INTO ad_image (ad_id, image_id)
+VALUES 	(1, 1),
+          (2, 2),
+          (3, 3),
+          (4, 4);
+
+INSERT INTO ad_category (ad_id, category_id)
+VALUES	(1, 1),
+          (2, 2),
+          (3, 3),
+          (4, 4);
