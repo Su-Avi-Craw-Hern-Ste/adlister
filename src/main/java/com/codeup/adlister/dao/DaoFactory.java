d778b2e0d679d9039c38e0e1fc8de0cd2c4553ba
shanshan-su <78042275+shanshan-su@users.noreply.github.com>
2021-06-07 11:12:59 -0500

    feat: MySQLCategoriesDao and CreateAdServlet tested successfully

--- src/main/java/com/codeup/adlister/dao/DaoFactory.java
+++ src/main/java/com/codeup/adlister/dao/DaoFactory.java
@@ -2,6 +2,7 @@ package com.codeup.adlister.dao;
 
 public class DaoFactory {
     private static Ads adsDao;
+    private static Categories categoriesDao;
     private static Users usersDao;
     private static Config config = new Config();
 
@@ -12,10 +13,19 @@ public class DaoFactory {
         return adsDao;
     }
 
+    public static Categories getCategoriesDao() {
+        if (categoriesDao == null) {
+            categoriesDao = new MySQLCategoriesDao(config);
+        }
+        return categoriesDao;
+    }
+
     public static Users getUsersDao() {
         if (usersDao == null) {
             usersDao = new MySQLUsersDao(config);
         }
         return usersDao;
     }
+
+
 }
